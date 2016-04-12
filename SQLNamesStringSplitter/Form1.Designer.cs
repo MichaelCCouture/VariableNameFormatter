bcd76ccd3c0e1aef5447e733cff38d34f81f4e7f
Michael Couture <mcouture@PC-00224D4FDFC1.conestogac.on.ca>
2016-04-12 11:54:48 -0400

    Added Files

---
+++ SQLNamesStringSplitter/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace SQLNamesStringSplitter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.chkCapitalize = new System.Windows.Forms.CheckBox();
+            this.chkAtSign = new System.Windows.Forms.CheckBox();
+            this.chkCommaSeparated = new System.Windows.Forms.CheckBox();
+            this.chkUnderscores = new System.Windows.Forms.CheckBox();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.txtInput = new System.Windows.Forms.RichTextBox();
+            this.chkUpdateFormat = new System.Windows.Forms.CheckBox();
+            this.chkSprocGetFormat = new System.Windows.Forms.CheckBox();
+            this.chkSprocUpdateFormat = new System.Windows.Forms.CheckBox();
+            this.btnResetText = new System.Windows.Forms.Button();
+            this.chkDivTagFormat = new System.Windows.Forms.CheckBox();
+            this.chkSetTextboxFormat = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // chkCapitalize
+            // 
+            this.chkCapitalize.AutoSize = true;
+            this.chkCapitalize.Location = new System.Drawing.Point(13, 13);
+            this.chkCapitalize.Name = "chkCapitalize";
+            this.chkCapitalize.Size = new System.Drawing.Size(116, 17);
+            this.chkCapitalize.TabIndex = 0;
+            this.chkCapitalize.TabStop = false;
+            this.chkCapitalize.Text = "Capitalize first letter";
+            this.chkCapitalize.UseVisualStyleBackColor = true;
+            // 
+            // chkAtSign
+            // 
+            this.chkAtSign.AutoSize = true;
+            this.chkAtSign.Location = new System.Drawing.Point(13, 37);
+            this.chkAtSign.Name = "chkAtSign";
+            this.chkAtSign.Size = new System.Drawing.Size(95, 17);
+            this.chkAtSign.TabIndex = 1;
+            this.chkAtSign.TabStop = false;
+            this.chkAtSign.Text = "@ sign at front";
+            this.chkAtSign.UseVisualStyleBackColor = true;
+            // 
+            // chkCommaSeparated
+            // 
+            this.chkCommaSeparated.AutoSize = true;
+            this.chkCommaSeparated.Location = new System.Drawing.Point(13, 61);
+            this.chkCommaSeparated.Name = "chkCommaSeparated";
+            this.chkCommaSeparated.Size = new System.Drawing.Size(111, 17);
+            this.chkCommaSeparated.TabIndex = 2;
+            this.chkCommaSeparated.TabStop = false;
+            this.chkCommaSeparated.Text = "Comma separated";
+            this.chkCommaSeparated.UseVisualStyleBackColor = true;
+            // 
+            // chkUnderscores
+            // 
+            this.chkUnderscores.AutoSize = true;
+            this.chkUnderscores.Location = new System.Drawing.Point(13, 85);
+            this.chkUnderscores.Name = "chkUnderscores";
+            this.chkUnderscores.Size = new System.Drawing.Size(161, 17);
+            this.chkUnderscores.TabIndex = 3;
+            this.chkUnderscores.TabStop = false;
+            this.chkUnderscores.Text = "Underscores between words";
+            this.chkUnderscores.UseVisualStyleBackColor = true;
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(54, 242);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(75, 32);
+            this.btnSubmit.TabIndex = 4;
+            this.btnSubmit.TabStop = false;
+            this.btnSubmit.Text = "Format";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(180, 11);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(291, 324);
+            this.txtInput.TabIndex = 6;
+            this.txtInput.Text = "";
+            // 
+            // chkUpdateFormat
+            // 
+            this.chkUpdateFormat.AutoSize = true;
+            this.chkUpdateFormat.Location = new System.Drawing.Point(13, 109);
+            this.chkUpdateFormat.Name = "chkUpdateFormat";
+            this.chkUpdateFormat.Size = new System.Drawing.Size(110, 17);
+            this.chkUpdateFormat.TabIndex = 7;
+            this.chkUpdateFormat.TabStop = false;
+            this.chkUpdateFormat.Text = "Full update format";
+            this.chkUpdateFormat.UseVisualStyleBackColor = true;
+            // 
+            // chkSprocGetFormat
+            // 
+            this.chkSprocGetFormat.AutoSize = true;
+            this.chkSprocGetFormat.Location = new System.Drawing.Point(13, 133);
+            this.chkSprocGetFormat.Name = "chkSprocGetFormat";
+            this.chkSprocGetFormat.Size = new System.Drawing.Size(97, 17);
+            this.chkSprocGetFormat.TabIndex = 8;
+            this.chkSprocGetFormat.Text = "DAL get format";
+            this.chkSprocGetFormat.UseVisualStyleBackColor = true;
+            // 
+            // chkSprocUpdateFormat
+            // 
+            this.chkSprocUpdateFormat.AutoSize = true;
+            this.chkSprocUpdateFormat.Location = new System.Drawing.Point(13, 157);
+            this.chkSprocUpdateFormat.Name = "chkSprocUpdateFormat";
+            this.chkSprocUpdateFormat.Size = new System.Drawing.Size(115, 17);
+            this.chkSprocUpdateFormat.TabIndex = 9;
+            this.chkSprocUpdateFormat.Text = "DAL update format";
+            this.chkSprocUpdateFormat.UseVisualStyleBackColor = true;
+            // 
+            // btnResetText
+            // 
+            this.btnResetText.Location = new System.Drawing.Point(54, 280);
+            this.btnResetText.Name = "btnResetText";
+            this.btnResetText.Size = new System.Drawing.Size(75, 32);
+            this.btnResetText.TabIndex = 10;
+            this.btnResetText.Text = "Reset Text";
+            this.btnResetText.UseVisualStyleBackColor = true;
+            this.btnResetText.Click += new System.EventHandler(this.btnResetText_Click);
+            // 
+            // chkDivTagFormat
+            // 
+            this.chkDivTagFormat.AutoSize = true;
+            this.chkDivTagFormat.Location = new System.Drawing.Point(13, 180);
+            this.chkDivTagFormat.Name = "chkDivTagFormat";
+            this.chkDivTagFormat.Size = new System.Drawing.Size(92, 17);
+            this.chkDivTagFormat.TabIndex = 11;
+            this.chkDivTagFormat.Text = "Div tag format";
+            this.chkDivTagFormat.UseVisualStyleBackColor = true;
+            // 
+            // chkSetTextboxFormat
+            // 
+            this.chkSetTextboxFormat.AutoSize = true;
+            this.chkSetTextboxFormat.Location = new System.Drawing.Point(13, 204);
+            this.chkSetTextboxFormat.Name = "chkSetTextboxFormat";
+            this.chkSetTextboxFormat.Size = new System.Drawing.Size(118, 17);
+            this.chkSetTextboxFormat.TabIndex = 12;
+            this.chkSetTextboxFormat.Text = "Set Textbox Format";
+            this.chkSetTextboxFormat.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(487, 347);
+            this.Controls.Add(this.chkSetTextboxFormat);
+            this.Controls.Add(this.chkDivTagFormat);
+            this.Controls.Add(this.btnResetText);
+            this.Controls.Add(this.chkSprocUpdateFormat);
+            this.Controls.Add(this.chkSprocGetFormat);
+            this.Controls.Add(this.chkUpdateFormat);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.chkUnderscores);
+            this.Controls.Add(this.chkCommaSeparated);
+            this.Controls.Add(this.chkAtSign);
+            this.Controls.Add(this.chkCapitalize);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox chkCapitalize;
+        private System.Windows.Forms.CheckBox chkAtSign;
+        private System.Windows.Forms.CheckBox chkCommaSeparated;
+        private System.Windows.Forms.CheckBox chkUnderscores;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.RichTextBox txtInput;
+        private System.Windows.Forms.CheckBox chkUpdateFormat;
+        private System.Windows.Forms.CheckBox chkSprocGetFormat;
+        private System.Windows.Forms.CheckBox chkSprocUpdateFormat;
+        private System.Windows.Forms.Button btnResetText;
+        private System.Windows.Forms.CheckBox chkDivTagFormat;
+        private System.Windows.Forms.CheckBox chkSetTextboxFormat;
+    }
+}
+
---
+++ SQLNamesStringSplitter/Form1.cs
@@ -0,0 +1,355 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SQLNamesStringSplitter
+{
+    public partial class Form1 : Form
+    {
+        string originalInput;
+        bool capitalizeFirstLetter;
+        bool atSignAtFront;
+        bool commaSeparated;
+        bool underscoresBetweenWords;
+        bool updateFormat;
+        bool DALGetFormat;
+        bool DALUpdateFormat;
+        bool divTagFormat;
+        bool setTextboxFormat;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            originalInput = txtInput.Text;
+
+            capitalizeFirstLetter = chkCapitalize.Checked;
+            atSignAtFront = chkAtSign.Checked;
+            commaSeparated = chkCommaSeparated.Checked;
+            underscoresBetweenWords = chkUnderscores.Checked;
+            updateFormat = chkUpdateFormat.Checked;
+            DALUpdateFormat = chkSprocGetFormat.Checked;
+            DALGetFormat = chkSprocUpdateFormat.Checked;
+            divTagFormat = chkDivTagFormat.Checked;
+            setTextboxFormat = chkSetTextboxFormat.Checked;
+
+            if (updateFormat)
+            {
+                PrintUpdateFormat();
+                return;
+            }
+
+            if (DALGetFormat)
+            {
+                PrintDALGetFormat();
+                return;
+            }
+
+            if (DALUpdateFormat)
+            {
+                PrintDALUpdateFormat();
+                return;
+            }
+
+            if (divTagFormat)
+            {
+                DivPrintFormat();
+                return;
+            }
+
+            if(setTextboxFormat)
+            {
+                SetTextBoxFormat();
+                return;
+            }
+
+
+            string textInput = Regex.Replace(txtInput.Text, @"\r\n?|\n", ",");
+
+            string[] input = textInput.Split(new char[] { ' ', ','});
+
+            input = input.Where(x => !string.IsNullOrEmpty(x)).ToArray();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (capitalizeFirstLetter)
+                {
+                    input[i] = input[i].First().ToString().ToUpper() + String.Join("", input[i].Skip(1));                   
+                }
+                else
+                {
+                    input[i] = input[i].First().ToString().ToLower() + String.Join("", input[i].Skip(1));             
+                }
+
+                if (atSignAtFront)
+                {
+                    if (input[i].First().ToString() != "@")
+                    {
+                        input[i] = "@" + input[i];
+                    }
+                }
+                else
+                {
+                    if (input[i][0].ToString() == "@")
+                    {
+                        input[i] = String.Join("", input[i].Skip(1));
+                    }
+                }
+
+                if (underscoresBetweenWords)
+                {
+                    for (int j = 1; j < input[i].Length; j++)
+                    {
+                        if (char.IsUpper(input[i][j]) && (!char.IsUpper(input[i][j - 1]) || !char.IsUpper(input[i][j + 1])))
+                        {
+                            input[i] = String.Join("_", input[i].Substring(0, j), input[i].Substring(j));
+                            j++;
+                        }
+                    }
+                    input[i] = input[i].ToLower();
+                }
+                else
+                {
+                    //is_plo_elgible
+                    for (int j = 1; j < input[i].Length; j++)
+                    {
+                        if (input[i][j].ToString() == "_")
+                        {
+                            input[i] = String.Join("", input[i].Substring(0, j), input[i].Substring(j+1, 1).ToUpper(), input[i].Substring(j+2));
+                            j++;
+                        }
+                    }
+                }
+            }
+
+            txtInput.Clear();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if(commaSeparated)
+                {
+                    if (i+1 == input.Length)
+                    {
+                        txtInput.Text += input[i];
+                    }
+                    else
+                    {
+                        txtInput.Text += input[i] + ", \n";
+                    }                    
+                }
+                else
+                {
+                    if (i+1 == input.Length)
+                    {
+                        txtInput.Text += input[i];
+                        //txtInput.Text += "public [v] "+ input[i] + " { get; set; }";
+                    }
+                    else
+                    {
+                        txtInput.Text += input[i] + "\n";
+                        //txtInput.Text += "public [v] " + input[i] + " { get; set; }" + "\n";
+                    }                    
+                }
+            }
+        }
+
+        public void PrintUpdateFormat()
+        {
+            string textInput = Regex.Replace(txtInput.Text, @"\r\n?|\n", ",");
+
+            string[] leftHalfInput = textInput.Split(new char[] { ' ', ',' });
+            string[] rightHalfInput = textInput.Split(new char[] { ' ', ',' }); ;
+
+            leftHalfInput = leftHalfInput.Where(x => !string.IsNullOrEmpty(x)).ToArray();
+            rightHalfInput = rightHalfInput.Where(x => !string.IsNullOrEmpty(x)).ToArray();
+
+            txtInput.Clear();
+
+            for (int i = 0; i < leftHalfInput.Length; i++)
+            {
+                leftHalfInput[i] = leftHalfInput[i].First().ToString().ToLower() + String.Join("", leftHalfInput[i].Skip(1));
+
+                rightHalfInput[i] = rightHalfInput[i].First().ToString().ToUpper() + String.Join("", rightHalfInput[i].Skip(1));
+
+                //isPLOElgible
+
+                for (int j = 1; j < leftHalfInput[i].Length; j++)
+                {
+                    if (char.IsUpper(leftHalfInput[i][j]) && (!char.IsUpper(leftHalfInput[i][j-1]) || !char.IsUpper(leftHalfInput[i][j+1])))
+                    {
+                        leftHalfInput[i] = String.Join("_", leftHalfInput[i].Substring(0, j), leftHalfInput[i].Substring(j));
+                        j++;
+                    }
+                }
+                leftHalfInput[i] = leftHalfInput[i].ToLower();
+
+
+                if (rightHalfInput[i].First().ToString() != "@")
+                {
+                    rightHalfInput[i] = "@" + rightHalfInput[i];
+                }
+
+                if (i + 1 == rightHalfInput.Length)
+                {
+                    txtInput.Text += leftHalfInput[i] + " = " + rightHalfInput[i];
+                }
+                else
+                {
+                    txtInput.Text += leftHalfInput[i] + " = " + rightHalfInput[i] + ", \n";
+                }    
+            }
+        }
+
+        public void PrintDALGetFormat()
+        {
+            string textInput = Regex.Replace(txtInput.Text, @"\r\n?|\n", ",");
+
+            string[] rightHalfInput = textInput.Split(new char[] { ' ', ',' });
+            string[] leftHalfInput = textInput.Split(new char[] { ' ', ',' }); ;
+
+            rightHalfInput = rightHalfInput.Where(x => !string.IsNullOrEmpty(x)).ToArray();
+            leftHalfInput = leftHalfInput.Where(x => !string.IsNullOrEmpty(x)).ToArray();
+
+            txtInput.Clear();
+
+            for (int i = 0; i < rightHalfInput.Length; i++)
+            {
+                rightHalfInput[i] = rightHalfInput[i].First().ToString().ToLower() + String.Join("", rightHalfInput[i].Skip(1));
+
+                leftHalfInput[i] = leftHalfInput[i].First().ToString().ToUpper() + String.Join("", leftHalfInput[i].Skip(1));
+
+                //isPLOElgible
+
+                if (leftHalfInput[i].First().ToString() != "@")
+                {
+                    leftHalfInput[i] = "@" + leftHalfInput[i];
+                }
+
+                txtInput.Text +=  "cmd.Parameters.AddWithValue(" + '"' + leftHalfInput[i] + '"' +", " + rightHalfInput[i] +"); \n";                
+            }
+        }
+
+        public void PrintDALUpdateFormat()
+        {
+            string textInput = Regex.Replace(txtInput.Text, @"\r\n?|\n", ",");
+
+            string[] rightHalfInput = textInput.Split(new char[] { ' ', ',' });
+            string[] leftHalfInput = textInput.Split(new char[] { ' ', ',' }); ;
+
+            rightHalfInput = rightHalfInput.Where(x => !string.IsNullOrEmpty(x)).ToArray();
+            leftHalfInput = leftHalfInput.Where(x => !string.IsNullOrEmpty(x)).ToArray();
+
+            txtInput.Clear();
+
+            for (int i = 0; i < rightHalfInput.Length; i++)
+            {
+                rightHalfInput[i] = rightHalfInput[i].First().ToString().ToLower() + String.Join("", rightHalfInput[i].Skip(1));
+
+                leftHalfInput[i] = leftHalfInput[i].First().ToString().ToUpper() + String.Join("", leftHalfInput[i].Skip(1));
+
+                //isPLOElgible
+
+                for (int j = 1; j < rightHalfInput[i].Length; j++)
+                {
+                    if (char.IsUpper(rightHalfInput[i][j]) && (!char.IsUpper(rightHalfInput[i][j - 1]) || !char.IsUpper(rightHalfInput[i][j + 1])))
+                    {
+                        rightHalfInput[i] = String.Join("_", rightHalfInput[i].Substring(0, j), rightHalfInput[i].Substring(j));
+                        j++;
+                    }
+                }
+                rightHalfInput[i] = rightHalfInput[i].ToLower();
+
+                txtInput.Text += "pageData." + leftHalfInput[i] + " = reader[" + '"' + rightHalfInput[i] + '"' + "].ToString(); \n"; 
+            }
+        }
+
+        public void DivPrintFormat()
+        {
+            string textInput = Regex.Replace(txtInput.Text, @"\r\n?|\n", ",");
+
+            string[] input = textInput.Split(new char[] { ' ', ',' });
+
+            input = input.Where(x => !string.IsNullOrEmpty(x)).ToArray();
+
+            txtInput.Text = "";
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                input[i] = input[i].First().ToString().ToUpper() + String.Join("", input[i].Skip(1));
+
+                //<div class="prettyForm-line">
+                //    <asp:Label ID="lblProvProgStandardsDate" runat="server" Text="Standard/Description Date" AssociatedControlID="txtProvProgStandardsDate"></asp:Label>
+                //    <asp:TextBox ID="txtProvProgStandardsDate" runat="server" CssClass="datePicker" aria-describedby="txtProvProgStandardsDateHint"></asp:TextBox>
+                //    <span id="txtProvProgStandardsDateHint" class="form-hint">Date of program standard or description.</span>
+                //</div>
+
+                txtInput.Text += "<div class=\"prettyForm-line\"> \n";
+                txtInput.Text += "<asp:Label ID=\"lbl" + input[i] + "\" runat=\"server\" Text=\"Standard/Description Date\" AssociatedControlID=\"txt" + input[i] + "\"></asp:Label> \n";
+                txtInput.Text += "<asp:TextBox ID=\"txt" + input[i] + "\" runat=\"server\" CssClass=\"datePicker\" aria-describedby=\"txt" + input[i]+ "Hint\"></asp:TextBox> \n";
+                txtInput.Text += "<span id=\"txt" + input[i] + "Hint\" class=\"form-hint\">Date of program standard or description.</span> \n";
+                txtInput.Text += "</div> \n\n";
+                
+                //txtInput.Text += "<tr> \n<td> \n";
+                //txtInput.Text += "<asp:Label ID=\"lblPrevious" + input[i] + "\" runat=\"server\" Text=\"" + input[i] + "\" AssociatedControlID=\"txtPrevious" + input[i] + "\"></asp:Label> \n";
+                //txtInput.Text += "<asp:TextBox ID=\"txtPrevious" + input[i] + "\" runat=\"server\" CssClass=\"editor editor--wide\" TextMode=\"MultiLine\"></asp:TextBox> \n";
+                //txtInput.Text += "</td> \n";
+
+
+                //txtInput.Text += "<td> \n" ;
+                //txtInput.Text += "<asp:Label ID=\"lbl" + input[i] + "\" runat=\"server\" Text=\"" + input[i] + "\" AssociatedControlID=\"txt" + input[i] + "\"></asp:Label> \n";
+                //txtInput.Text += "<asp:TextBox ID=\"txt" + input[i] + "\" runat=\"server\" CssClass=\"editor editor--wide\" TextMode=\"MultiLine\"></asp:TextBox> \n";
+                //txtInput.Text += "</td> \n</tr> \n";
+            }
+
+        }
+
+        public void SetTextBoxFormat()
+        {
+            string textInput = Regex.Replace(txtInput.Text, @"\r\n?|\n", ",");
+
+            string[] input = textInput.Split(new char[] { ' ', ',' });
+
+            input = input.Where(x => !string.IsNullOrEmpty(x)).ToArray();
+
+            txtInput.Text = "";
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                input[i] = input[i].First().ToString().ToUpper() + String.Join("", input[i].Skip(1));
+
+                //txtConnectionToPloDate.Text = checklist.ConnectionToPloDate == null ? "" : ((DateTime)checklist.ConnectionToPloDate).ToString("yyyy/MM/dd"); 
+                //txtProvProgStandardsDate.Text = template.StandardsDate == null ? "" : ((DateTime)template.StandardsDate).ToString("yyyy/MM/dd");
+                txtInput.Text += "txt" + input[i] + ".Text = checklist." + input[i] + " == null ? \"\" : ((DateTime)checklist." + input[i] + ").ToString(\"yyyy/MM/dd\"); \n";
+
+                //txtInput.Text += "string " + input[i].ToLower() + " = txt" + input[i] + ".Text; \n";
+
+                //txtInput.Text += "DateTime? " + input[i] + " = !string.IsNullOrWhiteSpace(txt" + input[i] + ".Text) ? DateTime.Parse(txt" + input[i] + ".Text) : (DateTime?) null; \n";
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.F)
+            {
+                btnSubmit.PerformClick();
+            }
+        }
+
+        private void btnResetText_Click(object sender, EventArgs e)
+        {
+            if (originalInput != null)
+            {
+                txtInput.Text = originalInput;
+            }
+        }
+    }
+}
